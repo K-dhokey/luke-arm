5b6433120baab98437c7a7662c2dae15b8fea37f
Kamakshi Dhokey <94119868+K-dhokey@users.noreply.github.com>
2022-08-25 11:55:54 +0530

    Update servocontrol.c

--- servocontrol.c
+++ servocontrol.c
@@ -10,7 +10,7 @@
 //You can get these value from the datasheet of servo you use, in general pulse width varies between 1000 to 2000 mocrosecond
 #define SERVO_MIN_PULSEWIDTH 1000 //Minimum pulse width in microsecond
 #define SERVO_MAX_PULSEWIDTH 2000 //Maximum pulse width in microsecond
-#define SERVO_MAX_DEGREE 90 //Maximum angle in degree upto which servo can rotate
+#define SERVO_MAX_DEGREE 91 //Maximum angle in degree upto which servo can rotate
 
 static void mcpwm_example_gpio_initialize(void)
 {
