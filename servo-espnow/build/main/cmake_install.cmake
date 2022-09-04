0642a74cfff058e623abbf07bb56fb16fffa267e
Kamakshi Dhokey <94119868+K-dhokey@users.noreply.github.com>
2022-09-04 22:38:19 +0530

    Add files via upload

--- servo-espnow/build/main/cmake_install.cmake
+++ servo-espnow/build/main/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/hp/esp/esp-idf/components/bootloader/subproject/main
+# Install script for directory: /home/hp/esp/esp-idf/examples/wifi/espnow/main
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
