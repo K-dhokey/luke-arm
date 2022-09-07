0da7367798b94a7f378295eeaa27bad48aa156a5
Kamakshi Dhokey <94119868+K-dhokey@users.noreply.github.com>
2022-09-07 11:24:23 +0530

    Update servo_espnow.c

--- servo-espnow/main/servo_espnow.c
+++ servo-espnow/main/servo_espnow.c
@@ -1,63 +1,43 @@
+//Receiver Code
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_now.h"
+#include "esp_wifi.h"
+#include "nvs_flash.h"
 #include "esp_attr.h"
-
 #include "driver/mcpwm.h"
 #include "soc/mcpwm_periph.h"
 
 
-#include <stdlib.h>
-#include <string.h>
-
-#include "esp_wifi.h"
-#include "esp_log.h"
-#include "nvs_flash.h"
-#include "esp_now.h"
-#define TAG "ESP_NOW"
-
-uint8_t esp_1[6] = {0x3c, 0x61, 0x5, 0x12, 0xde, 0x94};
-uint8_t esp_2[6] = {0x44, 0x17, 0x93, 0xe6, 0x34, 0x90};
-
-char *mac_to_str(char *buffer, uint8_t *mac)
+typedef struct struct_message 
 {
-  sprintf(buffer, "%02x%02x%02x%02x%02x%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-  return buffer;
-}
+  int a;
+} struct_message;
+ 
+// Create a structured object
+struct_message mydata;
 
-void on_sent(const uint8_t *mac_addr, esp_now_send_status_t status)
+ 
+// Callback function executed when data is received
+void on_receive(const uint8_t * mac, const uint8_t *incomingData, int len)
 {
-  char buffer[13];
-  switch (status)
-  {
-  case ESP_NOW_SEND_SUCCESS:
-    ESP_LOGI(TAG, "message sent to %s", mac_to_str(buffer,(uint8_t *) mac_addr));
-    break;
-  case ESP_NOW_SEND_FAIL:
-    ESP_LOGE(TAG, "message sent to %s failed", mac_to_str(buffer,(uint8_t *) mac_addr));
-    break;
+  if(mac == NULL || incomingData == NULL || len <= 0){
+    printf("Error while receving data");
+    return;
   }
+  memcpy(&mydata, incomingData, sizeof(mydata));
+  // printf("Data received: ");
+  printf("%d\n",len);
+  printf("%d\n",mydata.a);
+ 
 }
-
-void on_receive(const uint8_t *mac_addr, const uint8_t *data, int data_len)
-{
-  char buffer[13];
-  ESP_LOGI(TAG, "got message from %s", mac_to_str(buffer, (uint8_t *)mac_addr));
-
-  printf("message: %.*s\n", data_len, data);
-}
-
-//You can get these value from the datasheet of servo you use, in general pulse width varies between 1000 to 2000 mocrosecond
+ 
 #define SERVO_MIN_PULSEWIDTH 1000 //Minimum pulse width in microsecond
 #define SERVO_MAX_PULSEWIDTH 2000 //Maximum pulse width in microsecond
-#define SERVO_MAX_DEGREE 91 //Maximum angle in degree upto which servo can rotate
-
-
-
-
-
-    
+#define SERVO_MAX_DEGREE mydata.a //Maximum angle in degree upto which servo can rotate
 
 
 static void mcpwm_example_gpio_initialize(void)
@@ -84,6 +64,7 @@ static uint32_t servo_per_degree_init(uint32_t degree_of_rotation)
 /**
  * @brief Configure MCPWM module
  */
+
 void mcpwm_example_servo_control(void *arg)
 {
     uint32_t angle, count;
@@ -113,13 +94,6 @@ void mcpwm_example_servo_control(void *arg)
 
 void app_main(void)
 {
-  uint8_t my_mac[6];
-  esp_efuse_mac_get_default(my_mac);
-  char my_mac_str[13];
-  ESP_LOGI(TAG, "My mac %s", mac_to_str(my_mac_str, my_mac));
-  bool is_current_esp1 = memcmp(my_mac, esp_1, 6) == 0;
-  uint8_t *peer_mac = is_current_esp1 ? esp_2 : esp_1;
-
   nvs_flash_init();
   tcpip_adapter_init();
   ESP_ERROR_CHECK(esp_event_loop_create_default());
@@ -130,27 +104,15 @@ void app_main(void)
   ESP_ERROR_CHECK(esp_wifi_start());
 
   ESP_ERROR_CHECK(esp_now_init());
-  ESP_ERROR_CHECK(esp_now_register_send_cb(on_sent));
   ESP_ERROR_CHECK(esp_now_register_recv_cb(on_receive));
 
-  esp_now_peer_info_t peer;
-  memset(&peer, 0, sizeof(esp_now_peer_info_t));
-  memcpy(peer.peer_addr, peer_mac, 6);
-
-  esp_now_add_peer(&peer);
-
-  char send_buffer[250];
-
-  for (int i = 0; i < 2; i++)
-  {
-    sprintf(send_buffer, "Hello from %s message %d", my_mac_str, i);
-    ESP_ERROR_CHECK(esp_now_send(NULL, (uint8_t *)send_buffer, strlen(send_buffer)));
-    vTaskDelay(pdMS_TO_TICKS(1000));
-  }
-
-
     printf("Testing servo motor.......\n");
-    xTaskCreate(mcpwm_example_servo_control, "mcpwm_example_servo_control", 4096, NULL, 5, NULL);
-    ESP_ERROR_CHECK(esp_now_deinit());
-    ESP_ERROR_CHECK(esp_wifi_stop());
+    xTaskCreate(mcpwm_example_servo_control, "mcpwm_example_servo_control", 4096, NULL, 4, NULL);
+
+
+}
+
+void loop() 
+{
+  vTaskDelay(1000);
 }
